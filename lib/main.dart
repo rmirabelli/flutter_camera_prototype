1da098b0bbbfa27318ce3c1b759f317edb5fae88
Russell Mirabelli <russell.mirabelli@bottlerocketstudios.com>
2023-08-30 12:03:52 -0500

    more prototyping

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +33,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // we'll update this list, and use it to populate the gallery.
+  List<Image> images = [];
+  final ImagePicker picker = ImagePicker();
+
+  _selectPhotos() async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final List<XFile?> selectedImages =
+        await picker.pickMultiImage(requestFullMetadata: false);
+    for (final image in selectedImages) {
+      final name = image!.name;
+      image.saveTo('${directory.path}/$name');
+    }
+    setState(() {
+      // eventually set the list of images, so there's something meaningful as a result.
+    });
+  }
+
+  _takeNewPhoto() async {
+    final XFile? selectedImages = await picker.pickImage(
+        source: ImageSource.camera, requestFullMetadata: false);
+    print('$selectedImages.length');
+
+    setState(() {
+      // eventually set the list of images, so there's something meaningful as a result.
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,10 +77,26 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: FloatingActionButton(
+              onPressed: _selectPhotos,
+              tooltip: 'Choose photo',
+              child: const Icon(Icons.image),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              onPressed: _takeNewPhoto,
+              tooltip: 'Take photo',
+              child: const Icon(Icons.camera),
+            ),
+          ),
+        ],
       ),
     );
   }
